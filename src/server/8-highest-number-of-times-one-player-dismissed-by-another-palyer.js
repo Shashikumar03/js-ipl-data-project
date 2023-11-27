a8c07552020874b057cd0066a822646d74371006
ShashiKumar03 <shashikumarkushwaha3@gmail.com>
2023-11-27 10:06:34 +0530

    maximum times one player is dissmissed by another player

--- src/server/8-highest-number-of-times-one-player-dismissed-by-another-palyer.js
+++ src/server/8-highest-number-of-times-one-player-dismissed-by-another-palyer.js
@@ -2,6 +2,45 @@ function highestNumberOfTimesOnePlayerDismissedByAnotherPlayer(deliveryData) {
   if (deliveryData === undefined || !Array.isArray(deliveryData)) {
     return;
   }
-  
+  const answer = deliveryData.reduce((wicketByBowler, delivery) => {
+    const { bowler, batsman, player_dismissed } = delivery;
+
+    if (player_dismissed !== undefined && player_dismissed !== "") {
+      if (wicketByBowler[bowler]) {
+        if (wicketByBowler[bowler][player_dismissed]) {
+          wicketByBowler[bowler][player_dismissed] += 1;
+        } else {
+          wicketByBowler[bowler][player_dismissed] = 1;
+        }
+      } else {
+        wicketByBowler[bowler] = {};
+        wicketByBowler[bowler][player_dismissed] = 1;
+      }
+    }
+    return wicketByBowler;
+  }, {});
+    
+  let maxValue = 0;
+  let batsmanName = "";
+    let bowlerName = "";
+    
+  for (let key in answer) {
+    let a = answer[key];
+    for (let keyA in a) {
+      if (a[keyA] > maxValue) {
+        maxValue = a[keyA];
+        batsmanName = keyA;
+        bowlerName = key;
+      }
+    }
+  }
+
+  const bowlerAndNumberOfTimes = {
+    [bowlerName]: maxValue,
+  };
+  const numberOfTimesAPlayerDismissesByAnother = {
+    [batsmanName]: bowlerAndNumberOfTimes,
+  };
+    return numberOfTimesAPlayerDismissesByAnother;
 }
 module.exports = highestNumberOfTimesOnePlayerDismissedByAnotherPlayer;
--- test/testHighestNumberOfTimesOnePlayerDismissedByAnotherPlayer.js
+++ test/testHighestNumberOfTimesOnePlayerDismissedByAnotherPlayer.js
@@ -1,5 +1,14 @@
+const fs = require("fs");
+const path = require("path");
 const deliveryData = require("../deliveries.json");
 const highestNumberOfTimesOnePlayerDismissedByAnotherPlayer = require("../src/server/8-highest-number-of-times-one-player-dismissed-by-another-palyer");
+const filePath = path.resolve(
+  __dirname,
+  "../src/public/output/highestNumberOfTimesOnePlayerDismissedByANotherPlayer.json"
+);
 
-const highestDismissal = highestNumberOfTimesOnePlayerDismissedByAnotherPlayer(deliveryData);
+const highestDismissal =
+  highestNumberOfTimesOnePlayerDismissedByAnotherPlayer(deliveryData);
 console.log(highestDismissal);
+const jsonString = JSON.stringify(highestDismissal, null, 2);
+fs.writeFileSync(filePath, jsonString, "utf-8");
